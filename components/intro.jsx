07253ac3b4351debd37de496042f9c35adb26ce5
John Carmichael <jc@stackeducation.com>
2020-04-16 13:04:51 -0400

    The solution to this exercise

---
+++ components/intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default () => (
+  <>
+    <div className="name">John Carmichael</div>
+    <div className="introduction">
+      <p>
+        I am a product developer with over a decade of experience building best-of-class solutions.
+        With expertise in product development, software engineering, cyber security, and technical
+        operations, I have helped startups create products to delight their customers.
+      </p>
+      <p>
+        I have held positions as a Software Engineer, Security Engineer, Director of Operations,
+        and Vice President of Product & Engineering.
+      </p>
+    </div>
+  </>
+)
