ae3c12fac6cd5bc18f89a2d7a05956c8c4f15c07
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-05-20 22:19:35 -0400

    styles

--- components/about.jsx
+++ components/about.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default () => (
   <div>
     <div>ABOUT ME!</div>
-    <div>I try to make things on the internet</div>
+    <div>I try to make things on the internet!!</div>
   </div>
 )
--- public/javascript/bundle.js
+++ public/javascript/bundle.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (() => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, \"ABOUT ME!\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, \"I try to make things on the internet\")));\n\n//# sourceURL=webpack:///./components/about.jsx?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (() => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, \"ABOUT ME!\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"div\", null, \"I try to make things on the internet!!\")));\n\n//# sourceURL=webpack:///./components/about.jsx?");
 
 /***/ }),
 
